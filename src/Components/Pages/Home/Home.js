e3189b6f8883514158c861003e9f5f04ee042add
abir <abir.cse.cou@gmail.com>
2021-11-19 01:36:53 +0100

    Send Long url to server

--- src/Components/Pages/Home/Home.js
+++ src/Components/Pages/Home/Home.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import { Button, Col, Container, Form, FormControl, InputGroup, Row } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import DisplayTable from '../DisplayTable/DisplayTable';
+import Shortener from '../Shortener/Shortener';
+
 
 const Home = () => {
     return (
         <div>
-            <h1 className="mt-3 mb-5"> URL Shortener</h1>
             <Container>
-            <Form>
-                <Row className="align-items-center">
-                    <Col sm={3} className="my-1">
-                    <Form.Label htmlFor="inlineFormInputName" visuallyHidden>
-                        Name
-                    </Form.Label>
-                    <Form.Control id="inlineFormInputName" placeholder="Large URL" />
-                    </Col>
-                   
-                    
-                    <Col xs="auto" className="my-1">
-                    <Button type="submit">Submit</Button>
-                    </Col>
-                </Row>
-            </Form>
+                <Shortener></Shortener>
+                <DisplayTable></DisplayTable>
             </Container>
         </div>
     );
---
+++ src/Components/Pages/Shortener/Shortener.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import { Button, Col, Container, Form, Row } from 'react-bootstrap';
+
+
+const Shortener = () => {
+    let [url, setUrl] = useState({});
+    
+
+    const handleOnChange = (e) => {
+        const field = e.target.name;
+        const value = e.target.value;
+        
+        const newUrl ={...url};
+        newUrl[field] = value;
+        setUrl(newUrl);
+        
+        
+
+    }
+
+    const handleUrlSubmit = (e) => {
+        e.preventDefault();
+        
+        const data = {...url};
+
+        // send to Server
+        fetch('http://localhost:5000/shortUrl', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log("Sent to server : ", data);
+
+        })
+
+        
+        window.location.reload(false);
+
+    }
+
+    return (
+        <div>
+            <Container>
+                <h1 className="mt-3 mb-5"> URL Shortener</h1>
+                <Form onSubmit={handleUrlSubmit}>
+                    <Row className="align-items-center">
+                        <Col sm={8} className="my-1">
+                        
+                        <Form.Control 
+                        onChange={handleOnChange}
+                        name="longUrl"
+                        id="inlineFormInputName" placeholder="Long URL" />
+                        </Col>
+                    
+                        
+                        <Col xs="auto" className="my-1">
+                        <Button type="submit">Submit</Button>
+                        </Col>
+                    </Row>
+                </Form>
+            </Container>
+        </div>
+    );
+};
+
+export default Shortener;
